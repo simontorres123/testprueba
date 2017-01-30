0c46bd95e436866c71fb5c0e5fb9377098883924
AldoNerio <aldo.nerio@gmail.com>
2017-01-30 12:02:16 -0600

    Modificando el correo

--- coned/app/views/admin/EjemploEmail.blade.php
+++ coned/app/views/admin/EjemploEmail.blade.php
@@ -8,13 +8,13 @@
   </head>
   <body > 
     <section style="position: relative; width: 400px; height: 480px;
-        background-color: rgba(15, 70, 17, 0); border-radius: 20%;">
-      <h1  align="center" style="position: relative; color: rgb(51, 50, 62); top: 60px;
-        left:140px; font-style: arial;">Bienvenido a: </h1>
-      <img  align="center" style="position: relative;top: 55%; left: 30%;" class="img-responsive" src="http://conedmex.com/images/conedlogo%20copy.png" alt="" width="40%" height="25%" top="15%" }>
+        background-color: red; border-radius: 20%;">
+      <h1  align="center" style="position: relative; color: rgb(51, 50, 62); top: 100px;
+        left:0px; font-style: arial;">Bienvenido a: </h1>
+      <img  align="center" style="position: relative;top: 80px; left: -20px;" class="img-responsive" src="http://conedmex.com/images/conedlogo%20copy.png" alt="" width="40%" height="25%" top="15%" }>
       <br>
       <br>
-      <p  align="center" style="position: relative; left: 15%; color: rgb(51, 50, 62);">
+      <p  align="center" style="position: relative; left: 0%; color: rgb(51, 50, 62); top: 50px;">
       Hola,
 
         Gracias por registrarte 
@@ -28,10 +28,10 @@
         <br> 
         primer nivel.
       </p>
-      <section align="center"s tyle="position: relative; height: 15%; width: 55%;
-        background-color: #eef5ee; top: 20px ; left: 50px !important; border-radius: 10%;">
-        <ul style="position: relative; top: 10px;">
-          <a  size="50" style="color:black" title="Ingresar a la página" >Confirmar mi cuenta</a>
+      <section align="center" style="position: relative; height: 13%; width: 50%;
+     top: 40px ; left: 160px !important; border-radius: 10%;">
+        <ul style="position: relative; top: 10px; background-color:blue">
+          <a  size="50" style="color:black; position: relative; left:-10px;" title="Ingresar a la página" >Confirmar mi cuenta</a>
         </ul>
 
       </section>
--- coned/app/views/emails/emailV.blade.php
+++ coned/app/views/emails/emailV.blade.php
@@ -8,6 +8,46 @@
   </head>
   <body > 
     <section style="position: relative; width: 400px; height: 480px;
+        background-color: rgba(15, 70, 17, 0); border-radius: 20%;">
+      <h1  align="center" style="position: relative; color: rgb(51, 50, 62); top: 100px;
+        left:-50px; font-style: arial;">Bienvenido a: </h1>
+      <img  align="center" style="position: relative;top: 80px; left: 260px;" class="img-responsive" src="http://conedmex.com/images/conedlogo%20copy.png" alt="" width="40%" height="25%" top="15%" }>
+      <br>
+      <br>
+      <p  align="center" style="position: relative; left: -20%; color: rgb(51, 50, 62); top: 50px;">
+      Hola,
+
+        Gracias por registrarte 
+        <br>
+         comunidad,coned lo que mas nos interesa
+        <br> 
+        son nuestros clientes,
+        siendo una aplicacion facil
+        <br> de usar, ademas brindando una
+        seguridad de 
+        <br> 
+        primer nivel.
+      </p>
+      <section align="center" style="position: relative; height: 13%; width: 50%;
+     top: 40px ; left: 160px !important; border-radius: 10%;">
+        <ul style="position: relative; top: 10px; ">
+          <a  size="50" style="color:black; position: relative; left:50px;" title="Ingresar a la página" href="{{url()}}/base/{{$data['emails']}}/token/{{$data['token']}}" >Confirmar mi cuenta</a>
+        </ul>
+
+      </section>
+    </section>
+  </body>
+</html>
+<!-- <!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  </head>
+  <body > 
+    <section style="position: relative; width: 400px; height: 480px;
         background-color: #0f4611; border-radius: 30%;">
       <h1  align="center" style="position: relative; color: #eef5ee; top: 60px;
         left:140px; font-style: arial;">Bienvenido a: </h1>
@@ -37,4 +77,4 @@
       </section>
     </section>
   </body>
-</html>
+</html> -->
